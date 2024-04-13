12c42b0dacbaee4f83cc091b0cfab73f3d407e2d
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-13 11:48:03 +0400

    input style

--- src/components/ContactBody/index.jsx
+++ src/components/ContactBody/index.jsx
@@ -2,9 +2,9 @@ import "./index.css"
 const ContactBody = ({}) => {
   return (
     <div className="divInputs">
-      <input style={{paddingBottom: "5px", width: "500px"}} type="text" placeholder="name" />
-      <input style={{paddingBottom: "5px", width: "500px"}} type="text" placeholder="email" />
-      <input style={{paddingBottom: "350px ", width: "500px"}} type="text" placeholder="message..." />
+      <input type="text" placeholder="name" />
+      <input type="text" placeholder="email" />
+      <input className="inputMessage" style={{paddingBottom: "350px ", width: "500px"}} type="text" placeholder="message..." />
       <button>send</button>
     </div>
   );
