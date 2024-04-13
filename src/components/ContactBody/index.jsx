fed231b9f93e5a8a43d6943b2dae540c95932726
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-13 13:32:21 +0400

    is tam hazir

--- src/components/ContactBody/index.jsx
+++ src/components/ContactBody/index.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import "./index.css"
 const ContactBody = ({}) => {
+    const [clicked, setCliced] = useState(false)
+    const click = ()=>{
+        setCliced(true)
+    }
   return (
     <div className="divInputs">
       <input type="text" placeholder="name" />
       <input type="text" placeholder="email" />
       <input className="inputMessage" type="text" placeholder="message..." />
-      <button>send</button>
+      <button onClick={click} style={{backgroundColor: clicked? 'blue': ''}}>send</button>
     </div>
   );
 };
--- src/page/Contact/index.jsx
+++ src/page/Contact/index.jsx
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import PageContainer from "../../components/PageContainer";
 import ContactBody from "../../components/ContactBody";
